b39023718ca79f76c8169e4c33a0f0f0287a7213
Jantira Vongampai <67124242+JTVong@users.noreply.github.com>
2023-02-12 22:56:23 -0800

    optimize functions and add clearer error handler
    - update updateLastIDOnField to increatement lastId by total of added photo/ review 
    - replace promise.all for getting lastID of each added photo with one time calling updateLastID and map each available ID to each photo instead 
    - reduce time complexity on finding target characteristic to update in updateMeta, by removing double for loop to just one for loop
    - add more specific error handler to help locating where the error occures preceisely

--- model/reviews_service.js
+++ model/reviews_service.js
@@ -1,140 +1,234 @@
-const { Review, Meta, LastID } = require('./reviewsModel.js');
+const { Review, Meta, LastID } = require("./reviewsModel.js");
 
-const getLast = (type, callback) => {
-  const updatedField = type === 'review' ? 'review_id' : 'photo_id';
-  LastID.findOneAndUpdate({}, {$inc:{[updatedField]: 1}}, (err, result) => err ? callback(err, null) : callback(null, result[updatedField] + 1));
-};
-
-const updateMeta = (review, callback) => {
-  const {product_id, rating, recommend, characteristics} = review
-  Meta.findOne({product_id}).exec((err, result) => {
-    if (err) {
-      callback(err, null);
-    } else {
-      const {totalReviews} = result;
-      result.rating[rating] =  result.rating[rating] ? result.rating[rating]+1 : 1;
-      result.recommended[recommend] =  result.recommended[recommend] ? result.recommended[recommend]+1 : 1;
-
-      for (const key in result.characteristics) {
-        for (const char_id in characteristics) {
-          if ( result.characteristics[key].id === Number(char_id)) {
-            let value = result.characteristics[key]['value']
-            result.characteristics[key]['value'] = Number.parseFloat((value * totalReviews + characteristics[char_id])/ (totalReviews + 1)).toFixed(2)
-          }
+const updateLastIDOnField = (fieldToUpdate, incrementAmount, callback) => {
+  try {
+    LastID.findOneAndUpdate(
+      {},
+      { $inc: { [fieldToUpdate]: incrementAmount } },
+      (error, result) => {
+        if (error) {
+          callback(error, null);
+          throw new Error(
+            `An error occurred while updating lastID on ${fieldToUpdate} field`
+          );
+        } else {
+          callback(null, result[fieldToUpdate] + incrementAmount);
         }
       }
+    );
+  } catch (error) {
+    console.error(error);
+  }
+};
 
-      Meta.findOneAndUpdate({product_id}, {$set:{rating: result.rating, recommended: result.recommended, characteristics: result.characteristics}, $inc:{totalReviews: 1}}, (err, result) => {
-        err ? callback(err, null) : callback(null, result)
-      });
-    }
-  });
+const updateAverageValue = (
+  addedValue,
+  currentAverageValue,
+  totalReviews
+) =>
+  Number.parseFloat(
+    (currentAverageValue * totalReviews + addedValue) / (totalReviews + 1)
+  ).toFixed(2);
+
+const updateMeta = (review, callback) => {
+  try {
+    const { product_id, rating, recommend, characteristics } = review;
+    let remainingCharacteristicToUpdate =
+      Object.keys.values(characteristics).length;
+    Meta.findOne({ product_id }).exec((err, result) => {
+      if (err) {
+        callback(err, null);
+        throw new Error(
+          `An error occured while finding data to update meta data of product_id: ${product_id}`
+        );
+      } else {
+        // update rating score
+        result.rating[rating] = result.rating[rating]
+          ? result.rating[rating] + 1
+          : 1;
+        // update recommended score
+        result.recommended[recommend] = result.recommended[recommend]
+          ? result.recommended[recommend] + 1
+          : 1;
+        const productCharacteristics = result.characteristics;
+        let currentCharacteristicID;
+        // update characteristics
+        for (const characteristic in productCharacteristics) {
+          // stop iteration if all reviewed characteristics have been updated
+          if (remainingCharacteristicToUpdate === 0) {
+            break;
+          }
+          currentCharacteristicID = productCharacteristics[characteristic].id;
+          if (currentCharacteristicID in characteristics) {
+            productCharacteristics[characteristic]["value"] =
+            updateAverageValue(
+                characteristics[currentCharacteristicID],
+                productCharacteristics[characteristic]["value"],
+                result.totalReviews
+              );
+            remainingCharacteristicToUpdate--;
+          }
+        }
+
+        Meta.findOneAndUpdate(
+          { product_id },
+          {
+            $set: {
+              rating: result.rating,
+              recommended: result.recommended,
+              characteristics: result.characteristics,
+            },
+            $inc: { totalReviews: 1 },
+          },
+          (error, result) => {
+            if (error) {
+              callback(err, null);
+              throw new Error(
+                `An error occures while updating meta data of product_id: ${product_id}`
+              );
+            } else {
+              callback(null, result);
+            }
+          }
+        );
+      }
+    });
+  } catch (error) {
+    console.error(error);
+  }
 };
 
 module.exports = {
-  getReview: (product_id, page, count, sort, callback) => {
-    page = page || 1;
-    count = count || 5;
-    sort =  sort ? sort === 'helpful' ? {'helpfulness': -1} : {date: -1} : {};
-    Review.find({product_id, reported:false}).select('-_id -product_id -reviewer_email -__v').sort(sort).limit(page*count).lean().exec((err, result) => {
-      callback(null, {product_id, page, count, results: result.slice(page*count - count)});
-    });
+  getReview: (product_id, page = 1, count = 5, sort, callback) => {
+    try {
+      const sortedBy = {};
+      switch (sort) {
+        case "helpful":
+          sortedBy.helpfulness = -1;
+          break;
+        case "date":
+          sortedBy.date = -1;
+          break;
+        default:
+          throw new Error("Sort field is not found");
+      }
+      Review.find({ product_id, reported: false })
+        .select("-_id -product_id -reviewer_email -__v")
+        .sort(sortedBy)
+        .limit(page * count)
+        .lean()
+        .exec((error, result) => {
+          if (error) {
+            callback(error, null);
+            throw new Error(
+              `An error occurred while finding review of this product_id: ${product_id}`
+            );
+          } else {
+            callback(null, {
+              product_id,
+              page,
+              count,
+              results: result.slice(page * count - count),
+            });
+          }
+        });
+    } catch (error) {
+      console.error(error);
+    }
   },
 
-  getMeta:(product_id, callback) => {
-    Meta.findOne({product_id}).select('-_id -__v -totalReviews').exec((err, result) => {
-      err ? callback(err, null) : callback(null,result)
-    })
+  getMeta: (product_id, callback) => {
+    try {
+      Meta.findOne({ product_id })
+        .select("-_id -__v -totalReviews")
+        .exec((error, result) => {
+          if (error) {
+            callback(error, null);
+            throw new Error(
+              `An error occurred while finding data of this product_id: ${product_id}`
+            );
+          } else {
+            callback(null, result);
+          }
+        });
+    } catch (error) {
+      console.error(error);
+    }
   },
 
   addReview: (review, callback) => {
-    const {product_id, photos, name, email, characteristics, recommend, rating} = review;
-    review.reviewer_name = name;
-    review.reviewer_email = email;
-    review.helpfulness = 0;
-    review.response = 'null';
-    delete review.name;
-    delete review.email;
-
-    new Promise((res, rej) => {
-      updateMeta({product_id, recommend, rating, characteristics}, (err, result) => {
-        err ? rej(err) : res(result);
-      })
-    }).then(() => {
-      new Promise ((res, rej) => {
-        getLast('review', (err, result) => {
-          err ? rej(err) : res(result)
-        })
-      }).then((result)=> {
-        review = {id: result, ...review};
-        const promises = [];
-        const photoList = []
-        if(photos.length) {
-          photos.forEach(el => {
-            promises.push(
-              new Promise ((res, rej) => {
-                getLast('photo', (err, result) => {
-                  if (err) {
-                    rej(err)
-                  } else {
-                    photoList.push({id: result, url: el});
-                    res(photoList);
-                  }
-                })
-              })
-            )
-          });
-          Promise.all(promises).then(() => {
-            review.photos = photoList;
-            new Review(review).save().then(result => callback(null, result));
-          });
-        } else {
-          new Review(review).save().then(result => callback(null, result));
-        }
-      });
+    const defaultFieldsForNewReview = { helpfulness: 0, response: null };
+    const updateReviewField = ({ name, email, ...rest }) => ({
+      rest,
+      ...defaultFieldsForNewReview,
+      reviewer_name: name,
+      reviewer_email: email,
     });
+    const { product_id, photos, characteristics, recommend, rating } =
+      updateReviewField(review);
+
+    const updateReviewOnMeta = new Promise((response, reject) =>
+      updateMeta(
+        { product_id, recommend, rating, characteristics },
+        (error, result) => (error ? reject(error) : response(result))
+      )
+    );
+    const udpateLastIDReview = new Promise((response, reject) =>
+      updateLastIDOnField("review_id", 1, (error, lastReviewID) =>
+        error ? reject(error) : response(lastReviewID)
+      )
+    );
+
+    Promise.all([updateReviewOnMeta, udpateLastIDReview])
+      .then(([_, lastReviewID]) => {
+        review.id = lastReviewID;
+        if (photos.length) {
+          updateLastIDOnField(
+            "photo_id",
+            photos.length,
+            (error, lastPhotoID) => {
+              if (error) {
+                throw new Error(
+                  "An error occurred while updating lastID of photo"
+                );
+              } else {
+                // order photo id based on photo_url order in photos list
+                review.photos = photos.map((photo_url, index) => ({
+                  id: Number(lastPhotoID) - (photos.length - 1 - index),
+                  url: photo_url,
+                }));
+              }
+            }
+          );
+        }
+        new Review(review).save().then((result) => callback(null, result));
+      })
+      .catch((error) => {
+        console.error(error);
+      });
   },
 
-  updateField: (review_id, field, callback)=> {
-    const option = field === 'helpful' ? {$inc: {helpfulness: 1}} : {$set:{reported: true}};
-    Review.findOneAndUpdate({id: review_id}, option, (err, result) => {
-      err ? callback(err, null) : callback(null, 'updated');
-    })
-  }
-}
-
-
-/** All functions below are used for cleaning up data after each route test */
-const cleanUpID = () => {
-  LastID.updateOne({}, {$set:{review_id: 5774952, photo_id
-    :
-    9999}}, (err, result) => {console.log(result)})
-}
-
-const deleteReview = (id) => {
-  Review.deleteOne({id}, (err, result) => {
-    err ? console.log(err) : console.log(result);
-  })
-}
-
-const cleanUpMeta = () => {
-  Meta.findOneAndUpdate({product_id: 3}, {$set: {characteristics: {Fit: {id: 6, value: 0}, Length: {id: 7, value:0}, Comfort: {id: 8, value: 0}, Quality: {id: 9, value:0}} , rating:{}, recommended:{}, totalReviews:0}}, (err, result) => err ? console.log(err) : console.log(result))
-}
-
-const cleanUpHelpfultest = () => {
-  Review.findOneAndUpdate({id:3}, {$set:{helpfulness: 5}}, (err, result) => {
-    err ? console.log(err) : console.log(result)
-  })
+  updateField: (review_id, field, callback) => {
+    try {
+      let updateResult;
+      const updateAction = {};
+      switch (field) {
+        case "helpful":
+          updateResult = "Update helpful field succesfully!";
+          updateAction["$inc"] = { helpfulness: 1 };
+          break;
+        case "report":
+          updateResult = "Update report review succesfully!";
+          updateAction["$set"] = { reported: true };
+          break;
+        default:
+          throw new Error("Field not found");
+      }
+      Review.findOneAndUpdate({ id: review_id }, updateAction, (err, _) => {
+        err ? callback(err, null) : callback(null, updateResult);
+      });
+    } catch (error) {
+      console.error(error);
+    }
+  },
 };
-
-const cleanUpReportTest = () => {
-  Review.findOneAndUpdate({id:3}, {$set:{reported: false}}, (err, result) => {
-    err ? console.log(err) : console.log(result)
-  })
-}
-
-// cleanUpHelpfultest();
-// cleanUpReportTest();
-// cleanUpMeta();
-// cleanUpID();
-// deleteReview(5774953)
